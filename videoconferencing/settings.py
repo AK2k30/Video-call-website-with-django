632f30d2e3b52d8110ab7c81a08060a6ad44672b
akash <akashsunilsingh5555@gmail.com>
2023-07-08 15:59:16 +0530

    new

--- videoconferencing/settings.py
+++ videoconferencing/settings.py
@@ -128,3 +128,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
